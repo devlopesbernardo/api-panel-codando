05c5a321cd8b022eeaf4ed6f57119833d3982ff4
Bernardo <devlopesbernardo@gmail.com>
2021-01-14 11:09:32 -0300

    Trying async version of exec

--- src/controllers/installers.js
+++ src/controllers/installers.js
@@ -1,42 +1,57 @@
 const { json } = require('body-parser');
-const { exec } = require('child_process');
+//const { exec } = require('child_process');
 const express = require('express');
 const router = express.Router();
 const app = express();
+const util = require('util');
+const { response } = require('../../app');
+const exec = util.promisify(require('child_process').exec);
 
 app.use(express.json());
 
-app.post('/wp', (req, res) => {
+app.post('/wp', async (req, res) => {
   const { url, passwordAdmin, userAdmin, ssl, email } = req.body;
 
-  exec(
+  const { stdout, stderr } = await exec(
     `wo site create ${url} --wpfc --user=${userAdmin} --pass=${passwordAdmin} ${
       ssl ? '--letsencrypt ' : ''
     } --email=${email}`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-      }
-      console.log(stdout);
-      exec(`wo site update ${url} --le`, (error, stdout, stderr) => {
-        if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-        }
-        if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-        }
-        console.log(stdout);
-      });
-    },
   );
-  res.status(200).send('Deu certo!');
+  if (!stderr) {
+    res.send(stdout);
+    const { out, err } = await exec(`wo site update ${url} --le`);
+    if (!err) {
+      console.log(out);
+    }
+  }
+  // exec(
+  //   `wo site create ${url} --wpfc --user=${userAdmin} --pass=${passwordAdmin} ${
+  //     ssl ? '--letsencrypt ' : ''
+  //   } --email=${email}`,
+  //   (error, stdout, stderr) => {
+  //     if (error) {
+  //       console.log(`error: ${error.message}`);
+  //       return;
+  //     }
+  //     if (stderr) {
+  //       console.log(`stderr: ${stderr}`);
+  //       return;
+  //     }
+  //     console.log(stdout);
+  //     exec(`wo site update ${url} --le`, (error, stdout, stderr) => {
+  //       if (error) {
+  //         console.log(`error: ${error.message}`);
+  //         return;
+  //       }
+  //       if (stderr) {
+  //         console.log(`stderr: ${stderr}`);
+  //         return;
+  //       }
+  //       console.log(stdout);
+  //     });
+  //   },
+  // );
+  // res.status(200).send('Deu certo!');
 });
 
 module.exports = app;
