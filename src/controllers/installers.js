d9fea3b2f5c53dbe0a17826ab3d6668518e40d32
Bernardo <devlopesbernardo@gmail.com>
2021-01-15 08:17:35 -0300

    Safely installing SSL as well

--- src/controllers/installers.js
+++ src/controllers/installers.js
@@ -17,47 +17,17 @@ app.post('/wp', async (req, res) => {
         ssl ? '--letsencrypt ' : ''
       } --email=${email}`,
     );
-    const { out, err } = await execa.command(
-      `wo site update ${url} --le --force`,
-    );
-    if (!err) {
-      return res.send('SSL ativado!');
-    }
-    return res.send('Encontramos um erro', err.toString());
+    (async () => {
+      const { stdout, stderr } = await execa.command(
+        `wo site update ${url} --le --force`,
+      );
+      if (!stderr) {
+        return res.send('SSL ativado!', stdout);
+      }
+      return res.send('Encontramos um erro', stderr.toString());
+    })();
     return console.log(stdout.toString());
-    //console.log(stdout);
-    //console.log(stderr);
   })();
-  //await generatessl(url);
-
-  // exec(
-  //   `wo site create ${url} --wpfc --user=${userAdmin} --pass=${passwordAdmin} ${
-  //     ssl ? '--letsencrypt ' : ''
-  //   } --email=${email}`,
-  //   (error, stdout, stderr) => {
-  //     if (error) {
-  //       console.log(`error: ${error.message}`);
-  //       return;
-  //     }
-  //     if (stderr) {
-  //       console.log(`stderr: ${stderr}`);
-  //       return;
-  //     }
-  //     console.log(stdout);
-  //     exec(`wo site update ${url} --le`, (error, stdout, stderr) => {
-  //       if (error) {
-  //         console.log(`error: ${error.message}`);
-  //         return;
-  //       }
-  //       if (stderr) {
-  //         console.log(`stderr: ${stderr}`);
-  //         return;
-  //       }
-  //       console.log(stdout);
-  //     });
-  //   },
-  // );
-  res.status(200).send('Deu certo!');
 });
 
 module.exports = app;
